e071247a93166b4bcf6f13685c2871d614fd52b7
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-08-28 18:43:19 +0700

    road to certificate

--- Javascript_Essentials_1/JSE-MODULE-2/Section-data_types_type_casting_part2/section_practice.js
+++ Javascript_Essentials_1/JSE-MODULE-2/Section-data_types_type_casting_part2/section_practice.js
@@ -20,3 +20,82 @@ let person = {};
 person.name = "Bella";
 person.surname = "Josh";
 console.log(`name: ${person.name} and surname: ${person.surname}`);
+
+// Arrays
+// Question 3: We are setting up our small library of books about JavaScript programming. We have three books and want to prepare a list of them. We will store three pieces information about each book: title, author, and number of pages:
+
+//     Speaking JavaScript, Axel Rauschmayer, 460;
+//     Programming JavaScript Applications, Eric Elliott, 254;
+//     Understanding ECMAScript 6, Nicholas C. Zakas, 352.
+
+// Create an array of three objects representing the books. Each object must have the following properties: title, author, pages.
+let book = [
+  {
+    title: "Speaking Javascript",
+    author: "Axel Rauschamer",
+    pages: 460,
+  },
+  {
+    title: "Programming Javascript Application",
+    author: "Eric Elliot",
+    pages: 254,
+  },
+  {
+    title: "Undestanding ECMAScript 6",
+    author: "Nicholas C. Zakas",
+    pages: 352,
+  },
+];
+// console.log(book);
+
+// Question 4: Add a new book to the collection: Learning JavaScript Design Patterns, by Addy Osmani, 254 pages. Use the appropriate method to do this, which will attach the book at the end of the array. Display the length of the array and, in turn, all the book names in the collection.
+let newwBook = {
+  title: "Learning Javascript Design Patterns",
+  author: "Addy Osmani",
+  pages: 254,
+};
+book.push(newwBook);
+console.log(book);
+
+// Question 5: Use the slice command to copy the last two books to the new array.
+
+let selectedBook = book.slice(1, -1);
+console.log(selectedBook);
+
+// Question 6: The first book from the collection is lost in unexplained circumstances. You have already accepted the loss, so remove it from the array. Which method will you use for this purpose? Display the length of the array and all the names of the books from the collection in turn.
+book.shift();
+console.log(book.length);
+
+let sum = book[0].pages + book[1].pages + book[2].pages;
+console.log(sum);
+
+//
+//
+//
+//
+// ## LAB DATA TYPES
+
+let contacts = [
+  {
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk",
+  },
+  {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com",
+  },
+  {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@convallis.edu",
+  },
+];
+let newContacts = {
+  name: "Maise Haley",
+  phone: "0913 531 3030",
+  email: "risus.Quisque.@unar.ca",
+};
+contacts.push(newContacts);
+console.log(contacts);
