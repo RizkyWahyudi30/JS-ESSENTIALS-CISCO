dd2ef79a9eee3fcb002a5c24905c1462577ec028
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-08-31 09:23:34 +0700

    road to certification

--- Javascript_Essentials_1/JSE-MODULE-4/Loops/loops.js
+++ Javascript_Essentials_1/JSE-MODULE-4/Loops/loops.js
@@ -38,3 +38,65 @@ let counter2 = 1;
 while (!isOver2) {
   isOver2 = !confirm(`[${counter2++}] Continue the loop?`);
 }
+
+//
+//
+// # do ... while loop
+// The do ... while loop is very similar to the plain while loop, the main difference being that in a while loop, the condition is checked before each iteration, and in the do ... whileloop, the condition is checked after each iteration.
+
+// The syntax of the do ... while loop looks as follows:
+/**
+ *
+ * do {
+ *  code block
+ * } while (condition)
+ *
+ */
+
+let isOver3;
+let counter3 = 1;
+do {
+  isOver3 = !confirm(`[${counter3++}] Continue the loop?`);
+} while (!isOver);
+
+// In the next example, we demonstrate what we said earlier – a do ... while loop always iterates at least once:
+let condition = false;
+
+while (condition) {
+  console.log("A while loop iteration."); // never executed
+}
+do {
+  console.log("A do ... while loop iteration."); // executed once
+} while (condition);
+
+//
+//
+// # for loop
+// The for loop is part of the second type of loops, the one which is better in situations where we know how many times the loop should be executed (although this is not necessary). The syntax of the for loop is a bit more complicated and looks as follows:
+/**
+ *
+ * for (initialization; condition, increment) {
+ *  block of code
+ * }
+ *
+ */
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+// As shown in the syntax of the for loop, there are three expressions inside the parentheses. The let i = 0 is an initialization, i < 10 is a condition, and i++ is an increment.
+
+// Now let's try to rewrite the same example using the while loop:
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+// Let's move on to another simple example. Let's assume that we have a four-element array of integers at our disposal and our dream is to sum up all these numbers. There is nothing easier than to go through the array, taking the element one by one and adding them to the result. We just have to remember that before starting the loop, the result is 0.
+
+let values = [10, 30, 50, 100];
+let sum = 0;
+for (let i = 0; i < 4; i++) {
+  sum += values[i];
+}
+console.log(sum); // -> 190
